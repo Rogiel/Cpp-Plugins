295170b56bb099d7d11825cb17cc73828129f750
Rogiel Sulzbach <rogiel@rogiel.com>
2014-07-19 16:46:55 -0300

    Initial Commit

---
+++ C++Plugins/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++Plugins
+//
+//  Created by Rogiel Sulzbach on 7/19/14.
+//  Copyright (c) 2014 Rogiel Sulzbach. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
